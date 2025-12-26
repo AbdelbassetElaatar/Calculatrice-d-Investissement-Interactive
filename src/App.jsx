00a51ae84eeda95baad89b1147ab4bc0a8cdc082
Fatima MAZOUZ <fatimamazouz770@gmail.com>
2025-12-26 15:08:14 +0100

    Ajouter form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import Form from './Components/Form/Form';
+
 
 function App() {
+ 
 
   return (
-    <>
+    <div>
+
+      <Form/>
     
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
